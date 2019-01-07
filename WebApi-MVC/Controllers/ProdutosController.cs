5fa00e61c4cc01d8aabf220eb5d59751791b4886
Everton <everton.srs@gmail.com>
2019-01-07 21:20:51 -0200

    Versão_01

---
+++ WebApi-MVC/Controllers/ProdutosController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using WebApi_MVC.Models;
+
+namespace WebApi_MVC.Controllers
+{
+    public class ProdutosController : ApiController
+    {
+        readonly Produtos[] produtos = new Produtos[]
+        {
+            new Produtos{ Id = 1, Categoria = "Celular", Preco = 100.00 },
+            new Produtos{ Id = 2, Categoria = "Carro", Preco = 200.00 },
+            new Produtos{ Id = 3, Categoria = "Pneu", Preco = 150.00 },
+            new Produtos{ Id = 4, Categoria = "Caixa", Preco = 700.00 },
+            new Produtos{ Id = 5, Categoria = "Som", Preco = 600.00 },
+            new Produtos{ Id = 6, Categoria = "PC", Preco = 500.00 }
+        };
+
+
+        public IEnumerable<Produtos> GetTodosProdutos()
+        {
+            return produtos;
+        }
+
+        public IHttpActionResult GetProduto(int id)
+        {
+
+            var produto = produtos.FirstOrDefault(p => p.Id == id);
+
+            if (produto == null)
+            {
+                return NotFound();
+            }
+            return Ok(produto);           
+
+        }
+
+        public IEnumerable<Produtos> GetProdutoCategoria(string cat)
+        {
+            var produto = produtos.Where(p => string.Equals(p.Categoria, cat, StringComparison.OrdinalIgnoreCase));
+
+            return produto;
+        }
+
+
+
+
+    }
+}
---
+++ WebApi-MVC/Models/Produtos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi_MVC.Models
+{
+    public class Produtos
+    {
+        public int Id { get; set; }
+        public string Categoria { get; set; }
+        public double Preco { get; set; }
+
+    }
+}
